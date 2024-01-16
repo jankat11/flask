4dfae746b757a34c615c632793fcae0ae9416c98
jankat11 <96008036+jankat11@users.noreply.github.com>
2024-01-16 16:16:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from flask import Flask, jsonify, render_template
-import os
-
+from flask import Flask, render_template
+ 
 app = Flask(__name__)
-
-
-@app.route('/')
+ 
+ 
+@app.route("/")
 def index():
-    return render_template('index.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return render_template("index.html")
+ 
+  if __name__ == "__main__":
+    app.run()
