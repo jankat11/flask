1de25c4b079107b914d9f35f1df727b8fb0aaf32
jankat11 <96008036+jankat11@users.noreply.github.com>
2024-01-16 17:37:57 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-from flask import Flask, jsonify
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+@app.route("/")
+def hello_world():
+    return render_template("index.html")
 
 
 if __name__ == '__main__':
